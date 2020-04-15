55c1645e47b8bd3781ab53f9d98b965f9f16d3a0
Akinwale Habib <akinwalehabib@hotmail.com>
2020-04-15 22:48:51 +0100

    ch: create basic application model

--- server/models/applicationModel.js
+++ server/models/applicationModel.js
@@ -1,1 +1,23 @@
 var mongoose = require('mongoose');
+
+/**
+ * TODO:
+ * 
+ * Model relationship with Job and User models
+ * Mark required fields
+ * Add model functions to abstract CRUD functionality
+ */
+var ObjectId = mongoose.Schema.ObjectId;
+var schema = new mongoose.Schema({
+    job_id: ObjectId,
+    user_id: ObjectId,
+    hours_commitment_per_week: Number,
+    motivation: String,
+    status: String,
+    created_at: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Application', schema);
