1987b0cdf4d336084cabb252d49e115861ee2d9b
Akinwale Habib <akinwalehabib@hotmail.com>
2020-04-16 01:38:43 +0100

    ch: add authentication endpoint request validation schema

---
+++ server/config/validation-schemas/auth.js
@@ -0,0 +1,37 @@
+var auth = {
+    login: {
+        email: {
+            in: ['body'],
+            errorMessage: 'Invalid email',
+            isEmail: true
+        },
+        password: {
+            in: ['body'],
+            errorMessage: 'Invalid password',
+            isString: true
+        }
+    },
+    signup: {
+        email: {
+            in: ['body'],
+            errorMessage: 'Invalid email',
+            isEmail: true
+        },
+        password: {
+            in: ['body'],
+            errorMessage: 'Invalid password',
+            isString: true
+        }
+    },
+    profile: {
+        token: {
+            in: ['headers'],
+            errorMessage: 'Invalid authentication token in request header',
+            isAlphanumeric: true
+        }
+    }
+};
+
+module.exports = auth;
+
+
